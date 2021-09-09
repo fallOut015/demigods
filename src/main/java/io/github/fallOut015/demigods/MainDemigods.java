0398cb6519eb18a81b4ac06278ab0b4af0edfb53
fallOut015 <captainDoodles264@gmail.com>
2021-09-09 11:46:41 -0700

    initial commit

---
+++ src/main/java/io/github/fallOut015/demigods/MainDemigods.java
@@ -0,0 +1,37 @@
+package io.github.fallOut015.demigods;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(MainDemigods.MODID)
+public class MainDemigods {
+    public static final String MODID = "demigods";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public MainDemigods() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+    }
+    private void processIMC(final InterModProcessEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+    }
+}
